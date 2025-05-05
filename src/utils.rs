927090cd52fc414782858831fede56ac4186fbe0
Brandon George <darth.data410@gmail.com>
2025-05-05 16:38:57 -0500

    updates for ascii art

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,14 @@
 /// Prints the Ferris crab ASCII art with JasDB branding
 pub fn print_ferris() {
     println!(
-        r#"
+r#"
     _~^~^~_
 \\) /  o o  \\ (/
   '_   u   _'
   \\ '-----' /
 
      JasDB
- Powered by Rust!
-        "#
+  Powered by Rust!
+"#
     );
 }
